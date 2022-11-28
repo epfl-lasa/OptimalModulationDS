371dcfccfd7fd6dcf0877b09f7fdf38584ea43fc
Michael <inflex56@gmail.com>
2022-11-28 16:57:13 +0100

    torch fk + gpu + jit

--- python_scripts/ds_mppi/fk_num.py
+++ python_scripts/ds_mppi/fk_num.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 from plots import *
 
+@torch.jit.script
 def dh_transform(q, d, theta, a, alpha):
     """
     Denavit-Hartenberg transformation matrix.
@@ -11,56 +12,70 @@ def dh_transform(q, d, theta, a, alpha):
     ca = torch.cos(alpha)
     sq = torch.sin(q+theta)
     cq = torch.cos(q+theta)
-    T = torch.tensor([
-        [cq,        -sq,    0,      a],
-        [sq * ca,   cq*ca,  -sa,    -d*sa],
-        [sq * sa,   cq*sa,  ca,     d*ca],
-        [0,         0,      0,      1]
-    ])
+    # T = torch.tensor([
+    #     [cq,        -sq,    0.0,      a],
+    #     [sq * ca,   cq*ca,  -sa,    -d*sa],
+    #     [sq * sa,   cq*sa,  ca,     d*ca],
+    #     [0.0, 0.0, 0.0, 1.0]]).to(q.device, q.dtype)
+    t1 = torch.hstack((cq, -sq, torch.tensor(0.0).to(q.device), a))
+    t2 = torch.hstack((sq * ca, cq*ca, -sa, -d*sa))
+    t3 = torch.hstack((sq * sa, cq*sa, ca, d*ca))
+    t4 = torch.tensor((0.0, 0.0, 0.0, 1.0)).to(q.device)
+    T  = torch.vstack((t1, t2, t3, t4))
     return T
 
 
-def dh_fk(q, dh_params):
+@torch.jit.script
+def dh_fk(q: torch.Tensor, dh_params: torch.Tensor):
     """
     Forward kinematics for a robot with Denavit-Hartenberg parameters.
     """
     # Initialize the transformation matrix
-    T = [torch.eye(4)]
+    T = [torch.eye(4).to(q.device)]
     # Loop through each joint
     for i in range(len(q)):
         # Extract the parameters for this joint
-        d, theta, a, alpha = dh_params[i]
+        d = dh_params[i, 0]
+        theta = dh_params[i, 1]
+        a = dh_params[i, 2]
+        alpha = dh_params[i, 3]
         # Compute the transformation for this joint
         T_prev = T[-1]
         T.append(T_prev @ dh_transform(q[i], d, theta, a, alpha))
     return T
 
 
-def numeric_fk_model(q, dh_params, n_pts):
+@torch.jit.script
+def numeric_fk_model(q: torch.Tensor, dh_params: torch.Tensor, n_pts: int):
     """
     Caclulate positions of points on the robot arm.
     """
     # Compute the transformation matrices
     n_dof = len(q)
-    a = dh_params[:, 2]
     P_arr = dh_fk(q, dh_params)
     robot = dict()
-    robot['links'] = []
-    robot['pts_int'] = []
+    # robot['links'] = []
+    # robot['pts_int'] = []
+    links = []
+    pts_int = []
     # Initialize the points array
     # Loop through each joint
+    a = dh_params[:, 2]
     for i in range(n_dof):
-        p0 = torch.tensor([0, 0, 0])
-        p1 = torch.tensor([a[i+1], 0, 0])
-        lspan = torch.linspace(0, 1, n_pts).unsqueeze(1)
+        p0 = torch.tensor([0, 0, 0]).to(q.device)
+        p1 = torch.hstack((a[i+1], torch.tensor([0.0, 0.0]).to(q.device)))
+        lspan = torch.linspace(0, 1, n_pts).unsqueeze(1).to(q.device)
         v = torch.tile(p0, (n_pts, 1)) + torch.tile(p1, (n_pts, 1)) * lspan
+
         R = P_arr[i + 1][:3, :3]
         T = P_arr[i + 1][:3, 3]
         # Compute the position of the point for this link
         pts = (R @ v.transpose(0, 1)).transpose(0, 1) + T
-        robot['links'].append(pts)
-        robot['pts_int'].append(v)
-    return robot
+        # robot['links'].append(pts)
+        # robot['pts_int'].append(v)
+        links.append(pts)
+        pts_int.append(v)
+    return links, pts_int
 
 
 def dist_to_point(robot, y):
@@ -91,18 +106,18 @@ def main():
     dh_d = dh_a*0
     dh_theta = dh_a*0
     dh_params = (torch.vstack((dh_d, dh_theta, dh_a, dh_alpha)).T).to(**params)
-    robot = numeric_fk_model(q, dh_params, 10)
-    link_pts = robot['links']
-    r_h = init_robot_plot(link_pts, -10, 10, -10, 10)
-    c_h = plot_circ(np.array([8, 5]), 1)
+    link_pts = numeric_fk_model(q, dh_params, 10)
+    #link_pts = robot['links']
+    # r_h = init_robot_plot(link_pts, -10, 10, -10, 10)
+    # c_h = plot_circ(np.array([8, 5]), 1)
     for i in range(300):
         q = q+torch.tensor([0.01, 0.01]).to(**params)
         robot = numeric_fk_model(q, dh_params, 10)
-        upd_r_h(robot['links'], r_h)
-        dst = dist_to_point(robot, np.array([10, 0, 0]))
-        print(dst)
-        c_h.set_center(q)
-        plt.pause(0.0001)
+        # upd_r_h(robot['links'], r_h)
+        # dst = dist_to_point(robot, np.array([10, 0, 0]))
+        # print(dst)
+        # c_h.set_center(q)
+        # plt.pause(0.0001)
 
 if __name__ == '__main__':
     main()
--- python_scripts/ds_mppi/main.py
+++ python_scripts/ds_mppi/main.py
@@ -2,31 +2,44 @@ import numpy as np
 from fk_num import *
 from fk_sym import *
 from plots import *
+import numpy as np
+import torch
+import time
+
+#define tensor parameters (cpu or cuda:0)
+params = {'device': 'cpu', 'dtype': torch.float32}
+#params = {'device': 'cuda:0', 'dtype': torch.float32}
+
 
 def main():
     DOF = 2
-    q = np.zeros(DOF)
+    q = torch.zeros(DOF).to(**params)
     q[0] = np.pi/2
-    dh_a = np.array([0, 3, 3])
+    dh_a = torch.tensor([0, 3, 3]).to(**params)
     dh_alpha = dh_a*0
     dh_d = dh_a*0
     dh_theta = dh_a*0
-    dh_params = np.vstack((dh_d, dh_theta, dh_a, dh_alpha)).T
-    robot_num = numeric_fk_model(q, dh_params, 10)
+    dh_params = (torch.vstack((dh_d, dh_theta, dh_a, dh_alpha)).T).to(**params)
+    link_pts, _ = numeric_fk_model(q, dh_params, 10)
     robot_sym = symbolic_fk_model(q, dh_params)
-    r_h = init_robot_plot(robot_num['links'], -10, 10, -10, 10)
-    y_p = np.array([8, 5])
+    r_h = init_robot_plot(link_pts, -10, 10, -10, 10)
+    y_p = torch.tensor([8, 5]).to(**params)
     y_r = 1
     c_h = plot_circ(y_p, y_r)
 
-    for i in range(1000):
-        q = q+np.array([0.01, 0.01])
-        link_pts = numeric_fk_model(q, dh_params, 10)['links']
-        upd_r_h(link_pts, r_h)
-        y_p = y_p-np.array([0.01, 0.01])
-        c_h.set_center(y_p)
-        plt.pause(0.0001)
+    # for i in range(1000):
+    #     q = q+torch.tensor([0.01, 0.01]).to(**params)
+    #     link_pts = numeric_fk_model(q, dh_params, 10)['links']
+    #     upd_r_h(link_pts, r_h)
+    #     y_p = y_p-torch.tensor([0.01, 0.01]).to(**params)
+    #     c_h.set_center(y_p)
+    #     plt.pause(0.0001)
 
+    t0 = time.time()
+    for i in range(100000):
+        link_pts, _ = numeric_fk_model(q, dh_params, 10)
+    tf = time.time()
+    print('numeric_fk_model time: ', tf-t0)
 if __name__ == '__main__':
     main()
 
