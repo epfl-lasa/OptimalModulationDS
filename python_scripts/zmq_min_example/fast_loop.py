a92fe79f0cd62b558fd743235a3dc076ba50ece8
Michael <inflex56@gmail.com>
2023-02-20 14:57:02 +0100

    zmq example sync for slow and fast worker

--- python_scripts/zmq_min_example/fast_loop.py
+++ python_scripts/zmq_min_example/fast_loop.py
@@ -5,27 +5,38 @@ import zmq
 
 ## ZMQ setup
 context = zmq.Context()
-socket = context.socket(zmq.SUB)
-socket.setsockopt(zmq.CONFLATE, 1)
-socket.connect("tcp://localhost:1337")
-socket.setsockopt(zmq.SUBSCRIBE, b"")
+# socket to receive data from slow loop
+socket_receive = context.socket(zmq.SUB)
+socket_receive.setsockopt(zmq.CONFLATE, 1)
+socket_receive.connect("tcp://localhost:1337")
+socket_receive.setsockopt(zmq.SUBSCRIBE, b"")
+
+# socket to publish data to slow loop
+socket_send = context.socket(zmq.PUB)
+socket_send.bind("tcp://*:1338")
 
 t_init = time.time()
 iter = 0
-
+data = torch.zeros(2, 7)
 while True:
     ## Iteration Start
     t_start = time.time()
     iter += 1
+
     ## Iteration Body
-    #  Get the reply.
+    #  Get the input from slow loop.
     try:
-        data = socket.recv_pyobj(zmq.DONTWAIT)
+        data = socket_receive.recv_pyobj(zmq.DONTWAIT)
         print(f"Received reply {iter} \n {data}")
         print(info)
     except:
         pass
-    time.sleep(1/500)
+    # assume some work done (i.e. 1-step integration using data)
+    time.sleep(1/500)  # work is 500 Hz
+    data[1, 0] = iter
+    #now send updated state to slow loop
+    socket_send.send_pyobj(data)
+
     ## Iteration End
     t_end = time.time()
     t_iter = t_end - t_start
--- python_scripts/zmq_min_example/slow_loop.py
+++ python_scripts/zmq_min_example/slow_loop.py
@@ -4,21 +4,40 @@ import zmq
 
 
 ## ZMQ setup
+# socket to publish data to fast loop
 context = zmq.Context()
-socket = context.socket(zmq.PUB)
-socket.bind("tcp://*:1337")
+socket_send = context.socket(zmq.PUB)
+socket_send.bind("tcp://*:1337")
+# socket to receive data from fast loop
+socket_receive = context.socket(zmq.SUB)
+socket_receive.setsockopt(zmq.CONFLATE, 1)
+socket_receive.connect("tcp://localhost:1338")
+socket_receive.setsockopt(zmq.SUBSCRIBE, b"")
 
 t_init = time.time()
 iter = 0
 tensor_size = [2, 7]
+data = torch.zeros(tensor_size)
+
 while True:
     ## Iteration Start
     t_start = time.time()
     iter += 1
+
     ## Iteration Body
-    time.sleep(1/10)
-    data = iter * torch.ones(tensor_size)
-    socket.send_pyobj(data)
+    #  Get the input from fast loop.
+    try:
+        data = socket_receive.recv_pyobj(zmq.DONTWAIT)
+        print(f"Received reply {iter} \n {data}")
+        print(info)
+    except:
+        pass
+    # assume some work done (i.e. path-planning for state in data)
+    time.sleep(1/10) #  work is 10 Hz
+    data[0, 0] = iter
+    # now send updated data to the fast loop
+    socket_send.send_pyobj(data)
+
     ## Iteration End
     t_end = time.time()
     t_iter = t_end - t_start
